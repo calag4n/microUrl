55377f78f976ae670150c3f9f2b8cb1db701d7ea
calag4n <console.logan@gmail.com>
2020-10-28 13:04:08 +0100

    first commit

---
+++ server/routes/url.routes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const urlRoutes = express.Router()
+
+const controller = require('../controllers/url.controller')
+
+urlRoutes.get('/:slug', controller.getUrl)
+urlRoutes.get('/', controller.getAll)
+
+urlRoutes.post('/new', controller.postUrl)
+
+module.exports = urlRoutes
